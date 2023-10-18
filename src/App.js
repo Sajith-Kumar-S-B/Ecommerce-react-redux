152d7ccb3bf4957fff17bc21fb5f0ce868128c2a
Sajith-Kumar-S-B <sajithkumarsb1@gmail.com>
2023-10-18 14:34:33 +0530

    v2

--- src/App.js
+++ src/App.js
@@ -1,13 +1,24 @@
+import { Route,  Routes } from 'react-router-dom';
 import './App.css';
-import './bootstrap.min.css'
 import Footer from './components/Footer';
 import Header from './components/Header';
-
+import Home from './Pages/Home';
+import Cart from './Pages/Cart';
+import Wishlist from './Pages/Wishlist';
 
 function App() {
   return (
     <>
     <Header/>
+    <Routes>
+      <Route path='/' element={<Home/>}/>
+      <Route path='/cart' element={<Cart/>}/>
+      <Route path='/wishlist' element={<Wishlist/>}/>
+
+
+      <Route/>
+
+    </Routes>
     <Footer/>
     </>
   );
--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,8 +1,27 @@
 import React from 'react'
+import {Nav,Navbar,Container,Badge} from 'react-bootstrap'
+import { Link } from 'react-router-dom'
 
 function Header() {
   return (
-    <div>Header</div>
+    <Navbar expand="lg" className="bg-body-tertiary position-fixed top-0 w-100 mb-5 z-1">
+    <Container>
+      <Navbar.Brand><Link to={'/'} style={{textDecoration:'none',color:'black',fontWeight:'bold'}}><i className="fa-solid fa-truck-fast me-2"></i>
+      E-Cart</Link></Navbar.Brand>
+      <Navbar.Toggle aria-controls="basic-navbar-nav" />
+      <Navbar.Collapse id="basic-navbar-nav">
+        <Nav className="ms-auto">
+          <Nav.Link className='btn border rounded'><Link className='d-flex justify-content-center align-items-center' to={'/wishlist'} style={{textDecoration:'none',color:'black'}}><i className="fa-solid fa-heart me-2" style={{color: "#FF5733"}}></i>
+          Wishlist
+          <Badge className='ms-2 rounded bg-secondary' >0</Badge></Link></Nav.Link>
+          <Nav.Link className='btn border rounded ms-3'><Link className='d-flex justify-content-center align-items-center' to={'/cart'} style={{textDecoration:'none',color:'black'}}><i class="fa-solid fa-cart-shopping me-2" style={{color: "#C70039"}}></i>
+          Cart
+          <Badge className='ms-2 rounded' bg="secondary">0</Badge></Link></Nav.Link>
+         
+        </Nav>
+      </Navbar.Collapse>
+    </Container>
+  </Navbar>
   )
 }
 
